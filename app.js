9a1a4407e6a1ad6354f2a8b76ed247175d85b9e6
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-06-21 19:34:37 +0300

    all necessary folders/files created

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const usersRouter = require("./routes/user");
+const tasksRouter = require("./routes/tasks");
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -11,6 +14,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/users", usersRouter);
+app.use("/contacts", tasksRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
