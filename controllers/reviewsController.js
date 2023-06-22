3341cdcfdb5401b92070f2bc364aa1df3afe604c
Pribalt <vitalisvitalik@gmail.com>
2023-06-22 22:09:25 +0300

    change reviews

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,7 +1,8 @@
 const { Review } = require("..//models/review");
-// const { HttpError, asyncWrapper } = require("");
+const { HttpError } = require("..//helpers");
+const { controllerWrap } = require("..//decorators");
 
-const getAllReviews = asyncWrapper(async (req, res) => {
+const getAllReviews = controllerWrap(async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Review.find({})
@@ -11,43 +12,43 @@ const getAllReviews = asyncWrapper(async (req, res) => {
   res.json(result);
 });
 
-const getOwnerReview = asyncWrapper(async (req, res) => {
+const getOwnerReview = controllerWrap(async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Review.find({ owner }).limit(limit).skip(skip);
 
   if (!result) {
-    throw new HttpError(404);
+    throw HttpError(404);
   }
   res.json(result);
 });
 
-const addReview = asyncWrapper(async (req, res) => {
+const addReview = controllerWrap(async (req, res) => {
   const { _id: owner } = req.user;
   const result = await Review.create({ ...req.body, owner });
   res.json(result);
 });
 
-const updateCommentReview = asyncWrapper(async (req, res) => {
+const updateCommentReview = controllerWrap(async (req, res) => {
   const { id } = req.params;
   const result = await Review.findByIdAndUpdate(id, req.body, {
     new: true,
   });
 
   if (!result) {
-    throw new HttpError(404);
+    throw HttpError(404);
   }
 
   res.json(result);
 });
 
-const deleteReview = asyncWrapper(async (req, res) => {
+const deleteReview = controllerWrap(async (req, res) => {
   const { id } = req.params;
   const result = await Review.findByIdAndRemove(id);
 
   if (!result) {
-    throw new HttpError(404);
+    throw HttpError(404);
   }
 
   res.json(result);
