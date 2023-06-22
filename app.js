086ace7d1cf1af6e5c96ba9c347f660fda0051d8
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-06-22 11:17:10 +0300

    app edit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/users", usersRouter);
-app.use("/contacts", tasksRouter);
+app.use("/tasks", tasksRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
