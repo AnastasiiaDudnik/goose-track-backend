c0c118f270ba44d686b4ebc4b64663557b958b49
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-07-08 18:12:36 +0300

    add tasks statistics

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
-const { isValidId, validateBody, authenticate, validateQuery } = require("../middlewares");
 const {
-    addTask,
-    getMonthTasks,
-    getTaskById,
-    removeTask,
-    updateTask,
-    getWeekTasks
+  isValidId,
+  validateBody,
+  authenticate,
+  validateQuery,
+} = require("../middlewares");
+const {
+  addTask,
+  getMonthTasks,
+  getTaskById,
+  removeTask,
+  updateTask,
+  getWeekTasks,
+  getTasksStatistics,
 } = require("../controllers/tasksController");
 const { joiTaskSchema, joiDate, joiWeek } = require("../schemas/tasks");
 
@@ -18,8 +24,8 @@ router.get("/week", validateQuery(joiWeek), getWeekTasks);
 router.get("/:id", isValidId, getTaskById);
 router.get("/", validateQuery(joiDate), getMonthTasks);
 router.post("/", validateBody(joiTaskSchema), addTask);
+router.post("/statistics", getTasksStatistics);
 router.patch("/:id", isValidId, validateBody(joiTaskSchema), updateTask);
 router.delete("/:id", isValidId, removeTask);
-
 
 module.exports = router;
